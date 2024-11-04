12caf6be45800342e1bd3910354ab198d167f68d
Tung <sontungnguyen16@gmail.com>
2024-11-04 15:09:05 +0700

    Initial commit

---
+++ app/src/main/java/com/map/nguyensontung/hangman/GameState.kt
@@ -0,0 +1,9 @@
+package com.map.nguyensontung.hangman
+
+sealed class GameState {
+    class Running(val lettersUsed: String,
+                  val underscoreWord: String,
+                  val drawable: Int) : GameState()
+    class Lost(val wordToGuess: String) : GameState()
+    class Won(val wordToGuess: String) : GameState()
+}
